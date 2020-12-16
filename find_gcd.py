77407469c45eff474c84ca15008af30b01814600
Ramavath-Saxoniel <72451599+Ramavath-Saxoniel@users.noreply.github.com>
2020-12-16 20:55:27 +0530

    Delete find_gcd.py

--- find_gcd.py
+++
@@ -1,9 +0,0 @@
-def get_gcd(a, b):
-        least = min(a, b)
-        for i in range(1, least+1):
-                if a==0 or b==0:
-                        gcd=0
-                if a%i==0 and b%i==0:
-                        gcd=i
-        return gcd
-print(get_gcd(20, 24))
